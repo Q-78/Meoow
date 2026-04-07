c56d021de9c63d58a00630d9c36d1691c68e8200
Q <1431802729@qq.com>
2026-04-07 14:55:16 +0800

    update: 4.7update

--- Scripts/playground.gd
+++ Scripts/playground.gd
@@ -4,14 +4,26 @@ extends Node2D
 
 @onready var cat_node = $cat001
 @onready var bowl_node = $bowl
+@onready var feed_button = $FeedButton
+
+var is_feeding_flow_running: bool = false
 
 func _ready() -> void:
 	bowl_node.feeding_finished.connect(_on_bowl_feeding_finished)
 	cat_node.eat_finished.connect(_on_cat_eat_finished)
-	pass
+	feed_button.pressed.connect(_on_feed_button_pressed)
 
 func _process(delta: float) -> void:
 	pass
+
+func _on_feed_button_pressed() -> void:
+	if is_feeding_flow_running:
+		return
+	
+	is_feeding_flow_running = true
+	print("点击了喂食按钮")
+	
+	bowl_node.show_and_feed()
 
 func _on_bowl_feeding_finished() -> void:
 	var target_pos = $bowl/Marker2D.global_position
@@ -20,12 +32,7 @@ func _on_bowl_feeding_finished() -> void:
 
 func _on_cat_eat_finished() -> void:
 	bowl_node.set_empty()
-
-
-	
-	
-	
-
+	is_feeding_flow_running = false
 
 func _on_to_map_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/map.tscn")
