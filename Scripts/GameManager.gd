e9c16966cf93cb70daba095558bac4ac2b6f36f7
Eolande <1730368192@qq.com>
2026-04-13 14:35:21 +0800

    add square scene + interaction (stable version)

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -2,6 +2,16 @@ extends Node
 
 var intimacy = 0
 
+# Square butterfly persistence only between square <-> square_zoom.
+var persist_square_butterfly: bool = false
+var square_butterfly_state := {}
+var square_chase_time_left: float = -1.0
+
+func clear_square_butterfly_state() -> void:
+	persist_square_butterfly = false
+	square_butterfly_state = {}
+	square_chase_time_left = -1.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,4 +19,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
